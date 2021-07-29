cc1735c7208ae3832a46844c572191c7f96d17e8
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-29 23:57:39 +0530

    update : index.js -> perform CRUD ops w/ firestore

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
 const { credential } = admin;
 const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
 var serviceAccount = require("./serviceAccountConfigKey.json");
 
@@ -8,4 +9,22 @@ const adminAccount = initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 });
 
-console.log("AdminAccount-->", adminAccount);
+const db = getFirestore(adminAccount);
+
+db.collection("users2").doc("todos").update({
+	8: "test again",
+});
+
+db.collection("users2")
+	.doc("todos")
+	.get()
+	.then((res) => console.log("docs from FB Firestore-->", res.data()));
+
+db.collection("images")
+	.get()
+	.then((res) =>
+		// res.forEach((doc) =>
+		// 	console.log("gteiing docs from images collection-->", doc.data())
+		// )
+		console.log("res->", res)
+	);
