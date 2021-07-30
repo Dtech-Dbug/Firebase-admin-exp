ee52a702b3d179bf554ba0f86b41be4806c67114
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-30 21:41:43 +0530

    update : server.js => sent HTML as res to server endpoint

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+app.get("/", (req, res) => {
+	res.send("<h1>WOW! Learning to create a server, aint you?</h1>");
+});
+
 app.listen(PORT, () => {
 	console.log(`Server Running On Port : ${PORT}`);
 });
