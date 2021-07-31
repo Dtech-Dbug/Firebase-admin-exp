5e7b7d68978480dd0069dba96ddad59cb8cbecf4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-31 21:06:58 +0530

    update : index.js => createCustomToken

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -2,6 +2,7 @@ const admin = require("firebase-admin");
 const { credential } = admin;
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const { getAuth } = require("firebase-admin/auth");
 
 var serviceAccount = require("./serviceAccountConfigKey.json");
 
@@ -9,25 +10,35 @@ const adminAccount = initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 });
 
-const db = getFirestore(adminAccount);
+// const db = getFirestore(adminAccount);
 
-//create a collection in cloud firestore
-db.collection("Testing").doc("Stacks").set({
-	Platform: "Firebase",
-	Services: "Auth / Firestore / Storage",
-});
+// //create a collection in cloud firestore
+// db.collection("Testing").doc("Stacks").set({
+// 	Platform: "Firebase",
+// 	Services: "Auth / Firestore / Storage",
+// });
 
-//read the collection programatically
-db.collection("Testing")
-	.doc("Stacks")
-	.get()
-	.then((res) => console.table(res.data()))
+// //read the collection programatically
+// db.collection("Testing")
+// 	.doc("Stacks")
+// 	.get()
+// 	.then((res) => console.table(res.data()))
+// 	.catch((err) => console.log("Error->", err));
+
+// //update
+// db.collection("Testing").doc("Stacks").update({
+// 	otherStacks: "NodeJs",
+// });
+
+// //delete operations
+// db.collection("Testing").doc("Stacks").delete();
+
+//create custom token
+const u_id = "some-uid";
+
+getAuth()
+	.createUser()
+	.then((res) => {
+		console.log("resposen from custom token", res);
+	})
 	.catch((err) => console.log("Error->", err));
-
-//update
-db.collection("Testing").doc("Stacks").update({
-	otherStacks: "NodeJs",
-});
-
-//delete operations
-db.collection("Testing").doc("Stacks").delete();
